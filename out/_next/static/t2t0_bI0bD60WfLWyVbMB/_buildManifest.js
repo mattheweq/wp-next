b79cdfbf2221e8c5a8598ffb6a226127b891e211
mattheweq <matthew@olk1.com>
2023-09-22 16:55:43 +0100

    remove unused

--- out/_next/static/t2t0_bI0bD60WfLWyVbMB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c92a063aeeb18bd7.css","static/chunks/pages/index-c7fa3cd18bfa113d.js"],"/404":["static/chunks/pages/404-17cf14da5b0dfffb.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
