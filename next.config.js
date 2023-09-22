104c05601f4e2a88155a6d9b807fd9978fb6f790
mattheweq <matthew@olk1.com>
2023-09-22 16:14:49 +0100

    configure for gh pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-  basePath: '',
+
   // images: {
   //   domains: [process.env.IMG_DOMAIN],
   //   loader: 'custom',
   //   path: '/ '
   // },
+  basePath: '/wp-next', 
+  assetPrefix: '/wp-next/',
+
 };
