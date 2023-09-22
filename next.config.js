5176c01a576c09b180f24b8b5662cfe83fea3213
mattheweq <matthew@olk1.com>
2023-09-22 16:36:50 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-
-  // images: {
-  //   domains: [process.env.IMG_DOMAIN],
-  //   loader: 'custom',
-  //   path: '/ '
-  // },
-  basePath: '', 
-  assetPrefix: '/wp-next/',
-
+  basePath: ''
 };
